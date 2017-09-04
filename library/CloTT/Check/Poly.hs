8820af793a5a0200c980c6d79880d84a75e38a19
Adam Schønemann <adamschoenemann@gmail.com>
2017-09-04 16:44:30 +0200

    more work on poly-inf

--- library/CloTT/Check/Poly.hs
+++ library/CloTT/Check/Poly.hs
@@ -72,6 +72,9 @@ occursIn nm t = tyExcept $ OccursIn nm t
 nameNotFound :: Name -> TypingM a r
 nameNotFound nm = tyExcept $ NameNotFound nm
 
+otherErr :: String -> TypingM a r
+otherErr s = tyExcept $ Other s
+
 -- the typing monad
 newtype TypingM a r = TypingM { unTypingM :: RWST (TypingRead a) () TypingState (Except (TyExcept a)) r }
   deriving ( Functor
@@ -99,12 +102,14 @@ subtypeOf ty1@(A _ t1) ty2@(A _ t2) = subtypeOf' t1 t2 where
   -- <:->
   subtypeOf' (a1 :->: a2) (b1 :->: b2) = do
     ctx' <- b1 `subtypeOf` a1
-    local (const ctx') $ (substCtx ctx' a2) `subtypeOf'` (substCtx ctx' b2)
+    a2' <- substCtx ctx' a2
+    b2' <- substCtx ctx' b2
+    local (const ctx') $ a2' `subtypeOf` b2'
 
   -- <:\/L
-  subtypeOf' (Forall a (A _ t1)) t2 = do
-    ctx' <- local (\g -> g <+ marker a <+ exists a) $ (subst (exists a) a t1) `subtypeOf'` t2
-    pure $ dropTil (marker a) ctx'
+  subtypeOf' (Forall nm (A at1 t1)) t2 = do
+    ctx' <- local (\g -> g <+ marker nm <+ exists nm) $ (subst (A at1 $ TExists nm) nm (A at1 t1)) `subtypeOf` ty2
+    pure $ dropTil (marker nm) ctx'
 
   -- <:\/R
   subtypeOf' t1 (Forall a (A _ t2)) = do
@@ -129,8 +134,50 @@ subtypeOf ty1@(A _ t1) ty2@(A _ t2) = subtypeOf' t1 t2 where
         then ty1 `instR` ahat
         else nameNotFound ahat
 
-substCtx = undefined
-dropTil = undefined
-subst = undefined
+substCtx :: Eq a => TyCtx a -> Type a Poly -> TypingM a (Type a Poly)
+substCtx ctx (A a ty) = A a <$> 
+  case ty of
+    TFree x -> pure $ TFree x
+    TExists x 
+      | Exists x `isInContext` ctx -> pure $ TFree x
+      | otherwise                  -> otherErr $ "existential " ++ show x ++ " not in context during substitution"
+
+    t1 `TApp` t2 -> do
+      t1' <- substCtx ctx t1
+      t2' <- substCtx ctx t2
+      pure $ t1 `TApp` t2
+    
+    t1 :->: t2 -> do
+      t1' <- substCtx ctx t1
+      t2' <- substCtx ctx t2
+      pure $ t1 :->: t2
+    
+    Forall x t -> do
+      t' <- substCtx ctx t
+      pure $ Forall x t'
+
+dropTil :: Eq a => CtxElem a -> TyCtx a -> TyCtx a
+dropTil el (Gamma xs) = Gamma $ tl $ dropWhile (/= el) xs where
+  tl []     = []
+  tl (_:ys) = ys
+
+-- in subtyping it is used with Type' instead of Type.
+-- TODO: Figure out which one to use (Type' vs Type) and how
+-- to deal with annotations: subst them too or keep them?
+-- Subst them!
+subst :: Type a Poly -> Name -> Type a Poly -> Type a Poly
+subst x forY (A a inTy) = 
+  case inTy of
+    TFree y | y == forY -> x
+
+    TExists x -> error $ "Should never encounter existentials in subst"
+
+    t1 `TApp` t2 -> A a $ subst x forY t1 `TApp` subst x forY t2
+    
+    t1 :->: t2 -> A a $ subst x forY t1 :->: subst x forY t2
+    
+    -- TODO: Fix capture problems here
+    Forall x' t -> A a $ Forall x' (subst x forY t)
+
 instL = undefined
 instR = undefined
